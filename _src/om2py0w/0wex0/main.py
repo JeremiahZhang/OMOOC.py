fa5ead18b3532c8472d0b2544d2598860abe6323
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-17 14:08:54 +0800

    0wd6 interact

---
+++ _src/om2py0w/0wex0/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import sys
+
+print """Congratulations: You have invoked the script main.py
+
+-- I love coding in Python!
+-- Do you love it?
+
+Have fun in Python Star Trek!""" # 
+
+
