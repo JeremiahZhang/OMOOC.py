5241284c93c38d0d71f95c2787a15c0728c03144
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-17 15:11:33 +0800

    endoing succeed

--- _src/om2py0w/0wex0/main.py
+++ _src/om2py0w/0wex0/main.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import sys
+# print sys.getdefaultencoding()
+reload(sys) # 必须 reload
+sys.setdefaultencoding('utf-8')
 
 print """Congratulations: You have invoked the script main.py
 
@@ -14,7 +17,18 @@ keywords = sys.argv[1:] # The list of command line arguments passed to a Python 
 print keywords 
 print type(keywords) # 查看keyword 它是list 输入英文 print 英文
 
+for words in keywords:
+    # words = unicode(words, "ascii")
+    # print words
+    print type(words)
+    print words
+
 # 在 cmd line 中输入中文 如 我爱python
 # 问题： 中文 我爱 这部分 输出的是 一种编码形式 解决之 
 # 分析： 传递参数（中文字符）后 再次输出 可能没有解码还原为中文字符
 # 方案： google search 原来 在最初运用了 utf-8 编码 输出的时候 没有 unicoding 
+# keywords = unicode(keywords, "utf-8")
+# 但是不能解码list error need string or buffer
+# utf 解码错误
+#     words = words.defcode("ascii").encode("utf-8")
+# 在没有修改默认编码是 以上设置 也是错的
