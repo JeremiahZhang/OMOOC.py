8b08a6d6d50c868c8db0cbd54ef6eb654e7006ff
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-17 14:16:45 +0800

    invoking argv from cmd line

--- _src/om2py0w/0wex0/main.py
+++ _src/om2py0w/0wex0/main.py
@@ -8,4 +8,7 @@ print """Congratulations: You have invoked the script main.py
 
 Have fun in Python Star Trek!""" # 
 
+keywords = sys.argv[1:] # The list of command line arguments passed to a Python script
+# 命令行中 除脚本名 以外的所有参数都保存在 keywords 中
 
+print keywords # 查看keyword 它是list
