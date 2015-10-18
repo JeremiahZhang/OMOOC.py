8049af6b75a693dfb88d63e76e6c44dc717acaa2
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-18 15:13:54 +0800

    chinese decodining in raw_input used in shell

--- _src/om2py0w/0wex0/main.py
+++ _src/om2py0w/0wex0/main.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# 
 import sys
 # print sys.getdefaultencoding()
 reload(sys) # 必须 reload
@@ -12,11 +11,17 @@ print """Congratulations: You have invoked the script main.py
 
 Have fun in Python Star Trek!""" # 
 
-name = raw_input("Glad to write today's diary"
-    "\n Please give the name first:") + '.txt' 
-# 以上 提示中文 是乱码 pshell 会出现乱码
+# name = raw_input("请输入日志名: ".encode(sys.stdin.encoding)) + '.txt' # succeed
+name = raw_input("请输入日志名: ".encode(sys.stdout.encoding)) + '.txt' # succeed
+# name = raw_input("请输入日志名: ".decode('utf-8')) + ".txt"
+# name = raw_input("Glad to write today's diary"
+# "\n Please give the name first:") + '.txt' 
+# 以上 提示中文 是乱码 pshell 会出现乱码 cat 我的日志.txt
 # 以上 这输入中文名 也是乱码 
-# 待解决
+# 待解决 第二日 reboot之后 就自然解决了 amazing
+# win 的 reboot 真是强
+
+print name
 
 writer = open(name, "w") # if the textfile exist it'll be erased
 
@@ -51,7 +56,7 @@ for words in keywords:
 # 交互
 # ========== 调用 python脚本
 # ========== 中文输入
-# 在 cmd line 中输入中文 如 我爱python
+# 在 pshell cmd line 中输入中文 如 我爱python
 # 问题： 中文 我爱 这部分 输出的是 一种编码形式 解决之 
 # 分析： 传递参数（中文字符）后 再次输出 可能没有解码还原为中文字符
 # 方案： google search 原来 在最初运用了 utf-8 编码 输出的时候 没有 unicoding 
@@ -68,6 +73,11 @@ for words in keywords:
 # - 日期
 # - 中文编码 
 #    + 中文编码问题
+#        + name = raw_input("请输入日志名: ") + '.txt' 在pshell中 乱码
+#        + 分析
+# UnicodeDecodeError: 'utf8' codec can't decode byte 0xb2 in position 0: invalid start byte
+# 本身 sys.setdefaultencoding('utf-8') 是utf-8 编码的 在pshell中 raw_input("请输入日志名：") 中
+# 双引号中的 请输入日志名 出现乱码 璇疯緭鍏ユ棩蹇楀悕
 # =========== 回读文本
 # - 自动将过往日志打印？
 # 	+ 如何找到日志文件
